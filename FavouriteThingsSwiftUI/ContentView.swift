82de087ad4e096a672efea3c1f3133e2299f2318
Chen Sihan <schen@lcs.on.ca>
2021-01-10 19:26:35 -0500

    First section of favourite things(Vstack of image spacer and text along with height and width adjustment)

--- FavouriteThingsSwiftUI/ContentView.swift
+++ FavouriteThingsSwiftUI/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Image("Basketball").resizable()
+                .scaledToFit()
+                .padding([.top, .leading, .trailing])
+            Spacer()
+                .frame(width: 0.0, height: 57.0)
+            
+            
+            Text("Basketball is in my opinion the best hobby since it accomplishes goals of both exercising and fun . Another reason I like basketball is because there is no strict requirement of how many people can attend. I can either play myself or play in a team. I consider basketball an event that prevents me from staying in the house all day and that brings me joy.").padding([.leading, .bottom, .trailing])
+            
+            
+
+        }.navigationTitle("Favourite Things")
+                    
     }
 }
 
